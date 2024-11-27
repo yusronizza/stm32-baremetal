23816407b1f347fc8ffa3149a287e59de007e629
Yusron Izza F <yusronizzafaradisa@gmail.com>
2024-11-27 11:20:22 +0700

    Update main.c

--- 0_simple_led_blink/main.c
+++ 0_simple_led_blink/main.c
@@ -9,34 +9,34 @@
 //Get Base Peripheral Address
 #define PERIPHERAL_BASE_ADDR 		(0x40000000UL)
 #define AHB1_PERIPH_OFFSET 			(0x00020000UL)
-#define AHB1_BASE_ADDR				  (PERIPHERAL_BASE_ADDR + AHB1_PERIPH_OFFSET)
+#define AHB1_BASE_ADDR				(PERIPHERAL_BASE_ADDR + AHB1_PERIPH_OFFSET)
 
 //Get GPIO Address
-#define GPIOA_OFFSET				    (0x0000UL)
-#define GPIOA_BASE_ADDR				  (AHB1_BASE_ADDR + GPIOA_OFFSET)
+#define GPIOA_OFFSET				(0x0000UL)
+#define GPIOA_BASE_ADDR				(AHB1_BASE_ADDR + GPIOA_OFFSET)
 
 //Get RCC Address
-#define AHB1_RCC_OFFSET				  (0x3800UL)
+#define AHB1_RCC_OFFSET				(0x3800UL)
 #define AHB1_RCC_BASE_ADDR			(AHB1_BASE_ADDR + AHB1_RCC_OFFSET)
 
 //Get AHB1 Enable Register
-#define AHB1_RCC_AHB1ENR_OFFSET	(0x30UL)
+#define AHB1_RCC_AHB1ENR_OFFSET		(0x30UL)
 #define AHB1_RCC_AHB1ENR_ADDR		(AHB1_RCC_BASE_ADDR + AHB1_RCC_AHB1ENR_OFFSET)
 #define AHB1_RCC_AHB1ENR_R			(*(volatile unsigned int*)AHB1_RCC_AHB1ENR_ADDR)
 
 //Get GPIOA Mode Register
 #define GPIOA_MODER_OFFSET			(0x0UL)
-#define GPIOA_MODER_ADDR			  (GPIOA_BASE_ADDR + GPIOA_MODER_OFFSET)
+#define GPIOA_MODER_ADDR			(GPIOA_BASE_ADDR + GPIOA_MODER_OFFSET)
 #define GPIOA_MODER_ADDR_R			(*(volatile unsigned int*)GPIOA_MODER_ADDR)
 
 //Get GPIOA Output Data Register
-#define GPIOA_ODR_OFFSET			  (0x14UL)
-#define GPIOA_ODR_ADDR				  (GPIOA_BASE_ADDR + GPIOA_ODR_OFFSET)
-#define GPIOA_ODR_ADDR_R			  (*(volatile unsigned int*)GPIOA_ODR_ADDR)
+#define GPIOA_ODR_OFFSET			(0x14UL)
+#define GPIOA_ODR_ADDR				(GPIOA_BASE_ADDR + GPIOA_ODR_OFFSET)
+#define GPIOA_ODR_ADDR_R			(*(volatile unsigned int*)GPIOA_ODR_ADDR)
 
-#define GPIOAEN						      (1U<<0)
-#define PA5							        (1U<<5)
-#define LED_PIN						      PA5
+#define GPIOAEN						(1U<<0)
+#define PA5							(1U<<5)
+#define LED_PIN						PA5
 
 int main(void)
 {
